cb6957869455f69a04e81a3f99f9ba9440ef9806
Krishna Acondy <kc.acondy@outlook.com>
2023-08-09 22:58:24 +0100

    fix: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Head>
+        <link
+          rel='icon'
+          href='/icon?<generated>'
+          type='image/<generated>'
+          sizes='<generated>'
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
