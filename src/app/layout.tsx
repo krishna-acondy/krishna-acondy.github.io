d4ec1dd6e0b717546ba022e5bf7152da36f22d0a
Krishna Acondy <kc.acondy@outlook.com>
2023-08-21 16:48:31 +0100

    chore: change twitter image type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://krishna-acondy.io'),
   title: 'Krishna Acondy | Lead Full-stack Software Engineer',
   description:
-    'Krishna is an experienced software engineer with over 14 years in the industry, and has led projects in various companies across domains such as agritech, consumer lifestyle, healthtech, banking and financial services.',
+    'Krishna is an experienced software engineer with over 14 years in the industry, and has led projects in companies across agritech, consumer lifestyle, healthtech, banking and financial services domains.',
 };
 
 export default function RootLayout({
@@ -31,11 +31,11 @@ export default function RootLayout({
         <meta property='og:image:type' content='<generated>' />
         <meta property='og:image:width' content='<generated>' />
         <meta property='og:image:height' content='<generated>' />
-        <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:image' content='<generated>' />
         <meta name='twitter:image:type' content='<generated>' />
         <meta name='twitter:image:width' content='<generated>' />
         <meta name='twitter:image:height' content='<generated>' />
+        <meta name='twitter:card' content='summary_large_image' />
         <meta name='msvalidate.01' content='A1BAC9864AC268197BC79539F9195777' />
       </Head>
       <body className={firaCode.className}>
